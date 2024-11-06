abbe62cac8cebf5667b457ee6877fcd00949f951
billyhyunjun <157565164+billyhyunjun@users.noreply.github.com>
2024-11-06 14:19:57 +0900

    Delete orders directory

--- orders/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class CartSerializer(serializers.Serializer):
-    image = serializers.ImageField()
-    menu_name = serializers.CharField(max_length=100)
-    price = serializers.IntegerField()
-    quantity = serializers.IntegerField()
